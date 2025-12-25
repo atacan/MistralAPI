f7719ad1c208dc2acd426e9fa982c9157e8964fa
atacan <atacandur@icloud.com>
2025-12-25 14:22:02 +0100

    Add timeout parameter to createClient function for better control over request duration

--- Sources/MistralAPI/createClient.swift
+++ Sources/MistralAPI/createClient.swift
@@ -1,14 +1,15 @@
 import OpenAPIAsyncHTTPClient
 import OpenAPIRuntime
 import MistralAPITypes
+import NIOCore
 
-public func createClient(apiKey: String) throws -> Client {
+public func createClient(apiKey: String, timeout: TimeAmount = .hours(1)) throws -> Client {
 
     let serverURL = try Servers.Server1.url()
     let client = Client(
         serverURL: serverURL,
         configuration: .init(dateTranscoder: .iso8601WithFractionalSeconds),
-        transport: AsyncHTTPClientTransport(),
+        transport: AsyncHTTPClientTransport(configuration: .init(timeout: timeout)),
         middlewares: [
             AuthenticationMiddleware(apiKey: apiKey),
         ]
